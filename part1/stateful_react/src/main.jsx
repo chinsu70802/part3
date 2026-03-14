1f1667668efda1ac8ad49ddeaa693e747df73e7a
Raghavan Ramesh <raghavanramesh4@gmail.com>
2026-03-14 22:04:27 +0530

    remove incorrect part1 folder

--- part1/stateful_react/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App_2.jsx'
-
-createRoot(document.getElementById('root')).render(
-    <App />
-)
