9d12e4afdc9b36bcb3d5f032a1374a35474a7f49
Raghavan Ramesh <raghavanramesh4@gmail.com>
2026-03-22 11:47:41 +0530

    Edited .gitmodules

--- part2/courseinfo/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({parts}) => {
-
-  const sum = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
-  return (
-    <p><strong>total of {sum} exercises</strong></p>
-  )
-}
-
-export default Total
